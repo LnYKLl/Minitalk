7af735a37eef72fe3d38e8808fa5fe6ab7cb22af
LnYKLl <lennykiloul@outlook.com>
2025-03-28 14:55:21 +0100

    changed method

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 12:33:02 by lkiloul           #+#    #+#             */
-/*   Updated: 2025/03/28 14:23:56 by lkiloul          ###   ########.fr       */
+/*   Updated: 2025/03/28 14:54:47 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,19 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	blip;
-    t_data data;
-	i = 0;
-	if (argc != 2)
-		return (ft_printf("Error : Not enough arguments"), 0);
-    malloc(data);
-    if (!data)
-        return (0);
-	while (argv[1])
-	{
-		blip = ft_atoi(argv[1][i])
-        conversion(blip, data);
-	}
-	argv[0] = "test";
-}
+    int PID;
+    int i;
+    char message;
 
-int	conversion(int dec, t_data data)
-{
-	int i;
-	int j;
-	i = 128;
-	j = 0;
-	while (i >= 1)
-	{
-		data->tab[j] = i;
-		i = i / 2;
-		j++;
-	}
-    j = 0;
-    while (dec != 0)
+    i = 0;
+	if (argc != 2)
+        return (0);
+    PID = ft_atoi(argv[0]);
+    message = argv[1];
+    while (message[i])
     {
-        if (dec < data->tab[j])
-        {
-            data->bits[j] == 1
-            dec -= data->bits[j];
-        }
-        else
-            data->bits[j] == 0;
-        j++;
+        
     }
 }
+
+
